4692cf710919a37c937bb0097dedd480029b4640
Om <omlakkad0@email.com>
2026-05-21 16:54:51 +0530

    Added Google Search Console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,9 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+  verification: {
+    google: "googlea5b134fcce7efed0",
+  },
 };
 
 export default function RootLayout({
