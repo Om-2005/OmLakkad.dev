d4df0c4cfd65323b898613ba2afeaa0d3c659f31
Om <omlakkad0@email.com>
2026-05-21 17:14:23 +0530

    Fixed robots sitemap URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,8 +1,8 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://omlakkad.dev";
-  
+  const baseUrl = "https://om-lakkad-dev.vercel.app/";
+
   return {
     rules: {
       userAgent: "*",
