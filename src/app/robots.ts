b07035bd1c89783e0e391fea4c13768639c58997
Om <omlakkad0@email.com>
2026-05-21 17:20:43 +0530

    Added Google Search Console verification

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://om-lakkad-dev.vercel.app/";
+  const baseUrl = "https://om-lakkad-dev.vercel.app";
 
   return {
     rules: {
